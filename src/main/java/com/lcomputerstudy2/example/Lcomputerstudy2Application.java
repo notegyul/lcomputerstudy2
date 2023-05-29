19dc410820761514bd56a64fed008743eef1d94d
notegyul <104202618+notegyul@users.noreply.github.com>
2023-05-29 10:59:24 +0900

    Initial commit

---
+++ src/main/java/com/lcomputerstudy2/example/Lcomputerstudy2Application.java
@@ -0,0 +1,14 @@
+package com.lcomputerstudy2.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lcomputerstudy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lcomputerstudy2Application.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/lcomputerstudy2/example/Lcomputerstudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcomputerstudy2.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lcomputerstudy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
